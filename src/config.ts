8cde607d8336a93ff1301e23f2842532839cb960
hazel97 <omarmattia97@gmail.com>
2022-11-21 21:39:25 +0200

    feat: Added config module

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const envVarNames = [
+  "DB_HOST",
+  "DB_PORT",
+  "DB_USERNAME",
+  "DB_PASSWORD",
+  "DB_DBNAME",
+  "DB_DRIVER",
+];
+
+const validateEnvVars = (envVarNames: string[]): void => {
+  envVarNames.forEach((envVar) => {
+    if (process.env[envVar] === undefined) {
+      throw new Error("Invalid environment variables");
+    }
+  });
+};
+
+validateEnvVars(envVarNames);
+
+const config = {
+  db: {
+    host: process.env.DB_HOST as string,
+    driver: process.env.DB_DRIVER as string,
+    port: Number(process.env.DB_PORT) as number,
+    username: process.env.DB_USERNAME as string,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_DBNAME as string,
+  },
+};
+
+export default config;
