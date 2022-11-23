5d09b73a45a1974b041043cd4fe40218f89a5fdc
hazel97 <omarmattia97@gmail.com>
2022-11-23 20:17:30 +0200

    chore: update test script

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -1,17 +1,24 @@
 import console from "console";
 import process from "process";
 import testDB from "./testDB.js";
-import { execSync } from "child_process";
+import { exec } from "child_process";
 
 process.env.ENV = "test";
 
 async function preTest() {
   console.log("Pre-test scripts");
   await testDB.create();
+  await testDB.migrate();
 }
 
-async function testScript() {
-  execSync("node ./scripts/test/jasmineTests", {stdio: "inherit"});
+function testScript() {
+  const childProcess = exec("node ./scripts/test/jasmineTests");
+
+  childProcess.stdout.pipe(process.stdout);
+
+  return new Promise((resolve) => {
+    childProcess.on("exit", (exitCode) => resolve(exitCode));
+  });
 }
 
 async function postTest() {
--- scripts/test/testDB.js
+++ scripts/test/testDB.js
@@ -15,9 +15,27 @@ const db = knex({
   }
 });
 
+const testdb = knex({
+  client: env.DB_DRIVER,
+  connection: {
+    host: env.DB_HOST,
+    user: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    port: env.DB_PORT,
+    database: env.DB_TEST_DBNAME
+  }
+});
+
+
 async function create() {
+  await db.raw(`DROP DATABASE IF EXISTS ${env.DB_TEST_DBNAME};`);
   await db.raw(`CREATE DATABASE ${env.DB_TEST_DBNAME};`);
   console.log(`Created Database ${env.DB_TEST_DBNAME}`);
+}
+
+async function migrate() {
+  await testdb.migrate.latest();
+  await testdb.destroy();
 }
 
 async function drop() {
@@ -25,4 +43,4 @@ async function drop() {
   console.log(`Dropped Database ${env.DB_TEST_DBNAME}`);
 }
 
-export default { create, drop };
+export default { create, drop, migrate };
