82f9f7daaf1272ce0e5a0930b7dd93223f20871d
hazel97 <omarmattia97@gmail.com>
2022-11-25 14:09:02 +0200

    chore: update test script to work cross platform

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -2,7 +2,8 @@ import console from "console";
 import process from "process";
 import testDB from "./testDB.js";
 import { exec } from "child_process";
-
+import { sep } from "path";
+const _s = sep;
 process.env.ENV = "test";
 
 async function preTest() {
@@ -12,7 +13,7 @@ async function preTest() {
 }
 
 function testScript() {
-  const childProcess = exec("node ./scripts/test/jasmineTests");
+  const childProcess = exec(`node scripts${_s}test${_s}jasmineTests`);
   
   childProcess.stdout.pipe(process.stdout);
   childProcess.stderr.pipe(process.stderr);
