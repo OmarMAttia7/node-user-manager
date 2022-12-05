8bc3130b4cddff57cdaea74443a7a66d7875d234
hazel97 <omarmattia97@gmail.com>
2022-12-05 21:30:43 +0200

    refactor: refactor UserSchema

--- src/userManager/types/User.ts
+++ src/userManager/types/User.ts
@@ -1,16 +1,20 @@
-import { z } from "zod";
+import {
+  number as zNumber,
+  object as zObject,
+  string as zString,
+  infer as zInfer,
+} from "zod";
 
-const UserSchema = z.object({
-  id: z.number().optional(),
-  username: z
-    .string()
+const UserSchema = zObject({
+  id: zNumber().optional(),
+  username: zString()
     .min(5, "username can't be less than 5 characters")
     .max(30, "username can't be more than 30 characters"),
-  firstName: z.string().min(2).max(50),
-  lastName: z.string().min(2).max(50),
-  email: z.string().email(),
+  firstName: zString().min(2).max(50),
+  lastName: zString().min(2).max(50),
+  email: zString().email(),
 });
 
-export type User = z.infer<typeof UserSchema>;
+export type User = zInfer<typeof UserSchema>;
 
 export default UserSchema;
