1812faa3c074d31412040fe722767a2f4452a149
hazel97 <omarmattia97@gmail.com>
2022-12-06 15:46:57 +0200

    feat: add username character validation

--- src/userManager/types/User.ts
+++ src/userManager/types/User.ts
@@ -9,7 +9,8 @@ const UserSchema = zObject({
   id: zNumber().optional(),
   username: zString()
     .min(5, "username can't be less than 5 characters")
-    .max(30, "username can't be more than 30 characters"),
+    .max(30, "username can't be more than 30 characters")
+    .regex(/[a-zA-Z0-9_.]+/g, "username should only have letters, numbers, and underscore"),
   firstName: zString().min(2).max(50),
   lastName: zString().min(2).max(50),
   email: zString().email(),
